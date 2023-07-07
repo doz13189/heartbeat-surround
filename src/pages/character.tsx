9a3d3991b9ff328663fd104462bf42be1e52a3eb
Takahiro Kato <dozo13189@gmail.com>
2023-07-08 00:17:38 +0900

    update: ページを作成

---
+++ src/pages/character.tsx
@@ -0,0 +1,21 @@
+import "../app/globals.css";
+import styles from "@/app/page.module.css";
+import Link from "next/link";
+
+export default function Character() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.center}>
+        <h1>Character</h1>
+      </div>
+
+      <div className={styles.grid}>
+        <Link href="/" className={styles.card}>
+          <h2>
+            Back to home<span>-&gt;</span>
+          </h2>
+        </Link>
+      </div>
+    </main>
+  );
+}
