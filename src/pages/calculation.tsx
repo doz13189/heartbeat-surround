7b9afb780659a11ef4c78feef453075c9ff5581c
Takahiro Kato <dozo13189@gmail.com>
2023-07-08 00:19:34 +0900

    update: ページを作成 (#1)

---
+++ src/pages/calculation.tsx
@@ -0,0 +1,21 @@
+import "../app/globals.css";
+import styles from "@/app/page.module.css";
+import Link from "next/link";
+
+export default function Calculation() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.center}>
+        <h1>Damage Calculation</h1>
+      </div>
+
+      <div className={styles.grid}>
+        <Link href="/" className={styles.card}>
+          <h2>
+            Back to home<span>-&gt;</span>
+          </h2>
+        </Link>
+      </div>
+    </main>
+  );
+}
