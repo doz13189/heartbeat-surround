b0d8d7854cfc0a352c3596682ed9a8dc1fff977b
Takahiro Kato <dozo13189@gmail.com>
2023-07-09 20:12:55 +0900

    update: ページにSSRを実装

--- src/pages/character.tsx
+++ src/pages/character.tsx
@@ -1,12 +1,44 @@
 import "@/app/globals.css";
 import styles from "@/app/index.module.css";
+import { getCharacter } from "@/lib/character";
+import { InferGetStaticPropsType } from "next";
 import Link from "next/link";
 
-export default function Character() {
+export async function getServerSideProps() {
+  const allCharactersData = getCharacter();
+  return {
+    props: {
+      allCharactersData,
+    },
+  };
+}
+
+export default function Character({
+  allCharactersData,
+}: InferGetStaticPropsType<typeof getServerSideProps>) {
+  const dabi = allCharactersData[0].dabi;
+
   return (
     <main className={styles.main}>
       <div className={styles.center}>
         <h1>Character</h1>
+      </div>
+
+      <div className={styles.grid}>
+        <div>
+          <label>Sub Name</label>
+          <p>{String(dabi.subName)}</p>
+        </div>
+
+        <div>
+          <label>Power</label>
+          <p>{String(dabi.basicAbility.power)}</p>
+        </div>
+
+        <div>
+          <label>Advance Power</label>
+          <p>{String(dabi.advancePower.power)}</p>
+        </div>
       </div>
 
       <div className={styles.grid}>
