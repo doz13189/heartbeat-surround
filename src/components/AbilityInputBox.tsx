9ddee13625b430610e767c059c2d3c11cb320976
Takahiro Kato <dozo13189@gmail.com>
2024-01-14 01:05:29 +0900

    chore: delete unnecessary lines

--- src/components/AbilityInputBox.tsx
+++ src/components/AbilityInputBox.tsx
@@ -1,44 +1,33 @@
-import { Box, Button, HStack, Input, useNumberInput } from "@chakra-ui/react";
-import { SetStateAction } from "react";
+import { Box, Button, HStack, Input } from "@chakra-ui/react";
 import { Text } from "./parts/Text";
 
 type AbilityInputBoxProps = {
   label: string;
+  input: any;
   inc: any;
   dec: any;
-  input: any;
-  // value: number;
-  // setValue: (value: SetStateAction<number>) => void;
 };
 
 export default function AbilityInputBox({
   label,
-  // value,
-  // setValue,
+  input,
   inc,
   dec,
-  input,
 }: AbilityInputBoxProps) {
-  // const inc = getIncrementButtonProps();
-  // const dec = getDecrementButtonProps();
-  // const input = getInputProps();
-
   return (
     <Box pt={1} pb={1} px={4}>
       <Text color={"blackAlpha.700"}>{label}</Text>
       <HStack>
         <Input
           type="number"
-          id="basicAbilityPower"
-          name="basicAbilityPower"
+          id={label}
+          name={label}
           backgroundColor={"white"}
           color={"blackAlpha.700"}
           rounded={"lg"}
           required
           size={"sm"}
           {...input}
-          // value={value}
-          // onChange={(e) => setValue(Number(e.target.value))}
         />
         <Button backgroundColor={"white"} {...dec}>
           -
