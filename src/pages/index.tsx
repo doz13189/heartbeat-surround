044d289b802140216df3a961658831cf2a6579a8
Takahiro Kato <dozo13189@gmail.com>
2024-01-14 16:42:21 +0900

    feat: localizing into japanese (#23)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
 import { Heading } from "@/components/parts/Heading";
 import { Text } from "@/components/parts/Text";
-import { Box, LinkBox, LinkOverlay, Grid, Link } from "@chakra-ui/react";
+import {
+  Box,
+  LinkBox,
+  LinkOverlay,
+  Grid,
+  Link,
+  UnorderedList,
+  ListItem,
+  Alert,
+} from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
 
 export default function Index() {
   return (
@@ -16,10 +26,10 @@ export default function Index() {
           >
             <Heading pt={2} pb={2}>
               <LinkOverlay href="/calculation" color="blackAlpha.800">
-                Calculation
+                与ダメージ計算
               </LinkOverlay>
             </Heading>
-            <Text>Let&apos;s calculate the damage dealt to the opponent.</Text>
+            <Text>相手に与えるダメージ量を計算しよう!!</Text>
           </LinkBox>
         </Box>
         <Box color={"64C8EF"}>
@@ -31,16 +41,16 @@ export default function Index() {
             p={3}
           >
             <Heading pt={2} pb={2}>
-              <LinkOverlay href="/character">Character</LinkOverlay>
+              <LinkOverlay href="/question">よくある質問</LinkOverlay>
             </Heading>
-            <Text>Let&apos;s see the characters.</Text>
+            <Text>問い合わせがよくある類の質問を載せています</Text>
           </LinkBox>
         </Box>
 
         <Box p={2}>
           <Box>
             <Box p={2}>
-              <Heading color={"#F8784A"}>About this app</Heading>
+              <Heading color={"#F8784A"}>このアプリについて</Heading>
             </Box>
           </Box>
 
@@ -51,21 +61,34 @@ export default function Index() {
             p={4}
           >
             <Text>
-              When playing Ultra Impact, have you ever wondered how the amount
-              of damage a character deals to an opponent is determined ? This
-              app can calculate the damage your character does to your opponent.
+              僕のヒーローアカデミア
+              ウルトラインパクトの与ダメージ計算を行うアプリです。みなさんは、このような疑問をもったことはありませんか？
             </Text>
+            <Box pt={2} pb={2}>
+              <Text>
+                疑問1)
+                新しいキャラクターのアクションスキルはどのくらい与ダメージが出るの??
+              </Text>
+              <Text>
+                疑問2) どのメモリーを付けたら与ダメージが大きくなるの??
+              </Text>
+            </Box>
             <Text>
-              However, please be aware that it&apos;s not an official app, so it
-              may not be entirely accurate.
+              このアプリでは、キャラクターのステータスなどを入力することで、与ダメージを計算することができます。
+              アリーナやサークル祭でのバトルで、与ダメージを計算して勝利を掴みましょう!!
             </Text>
+            <Box pt={1} pb={1}>
+              <Text color={"#F8784A"}>
+                このアプリは、公式アプリではありません。計算される与ダメージの結果は、正確ではない場合があります。
+              </Text>
+            </Box>
           </Box>
         </Box>
 
         <Box p={2}>
           <Box>
             <Box p={2}>
-              <Heading color={"#F8784A"}>How to Use</Heading>
+              <Heading color={"#F8784A"}>使い方</Heading>
             </Box>
           </Box>
 
@@ -76,33 +99,190 @@ export default function Index() {
             p={4}
           >
             <Text>
-              If you wish to input your character&apos;s stats and calculate the
-              damage, please go to the{" "}
-              <Link
-                color="#FAC00F"
-                href={"/calculation"}
-                textDecoration={"underline"}
-              >
-                calculation
-              </Link>{" "}
-              page. If you want to check the damage for each character
-              individually, please go to the{" "}
-              <Link
-                color="#FAC00F"
-                href={"/calculation"}
-                textDecoration={"underline"}
-              >
-                character
-              </Link>{" "}
-              page.
+              与ダメージの計算は基本的には、以下の要素によって決まります。
             </Text>
+            <Box p={2}>
+              <UnorderedList fontSize="sm">
+                <ListItem>キャラクターのステータス</ListItem>
+
+                <ListItem>アクションスキルのダメージ率</ListItem>
+                <ListItem>味方キャラクターに付与されているバフ</ListItem>
+                <ListItem>敵キャラクターに付与されているバフ</ListItem>
+                <UnorderedList fontSize="sm"></UnorderedList>
+                <ListItem>敵キャラクターに付与されているデバフ</ListItem>
+              </UnorderedList>
+            </Box>
+
+            <Box>
+              <Box pt={1} pb={1}>
+                <Text color={"#F8784A"}>キャラクターのステータスについて</Text>
+              </Box>
+              <Text>
+                キャラクターのステータスとは、以下のステータスのことで、これらの合計値が基礎的な与ダメージの元となります。
+              </Text>
+              <Box p={2}>
+                <UnorderedList fontSize="sm">
+                  <ListItem>パワー</ListItem>
+                  <ListItem>アドバンスパワー</ListItem>
+                  <ListItem>メモリーのパワー</ListItem>
+                </UnorderedList>
+              </Box>
+              <Text>各ステータスは以下の画面から確認することができます。</Text>
+              <Box p={4}>
+                <Image
+                  src="/images/character.png"
+                  borderRadius="lg"
+                  alt="Character"
+                />
+              </Box>
+              <Box p={4}>
+                <Image
+                  src="/images/memory.png"
+                  borderRadius="lg"
+                  alt="Memory"
+                />
+              </Box>
+            </Box>
+
+            <Box>
+              <Box pt={1} pb={1}>
+                <Text color={"#F8784A"}>アクションスキルのダメージ率</Text>
+              </Box>
+              <Text>
+                アクションスキルに設定されているダメージの率です。以下の画面から確認することができます。
+              </Text>
+              <Box p={4}>
+                <Image src="/images/skill.png" borderRadius="lg" alt="Skill" />
+              </Box>
+            </Box>
+
+            <Box>
+              <Box pt={1} pb={1}>
+                <Text color={"#F8784A"}>
+                  味方キャラクターに付与されているバフ
+                </Text>
+              </Box>
+              <Text>与ダメージの計算に使用されるバフは以下です。</Text>
+              <Box p={2}>
+                <UnorderedList fontSize="sm">
+                  <ListItem>パワーアップ</ListItem>
+                  <ListItem>スキル威力アップ</ListItem>
+                  <ListItem>アクションスキル威力アップ</ListItem>
+                  <ListItem>クリティカル威力アップ</ListItem>
+                </UnorderedList>
+              </Box>
+              <Text>
+                適用されている各バフは以下の画面から確認することができます。
+              </Text>
+              <Box p={4}>
+                <Image
+                  src="/images/status.png"
+                  borderRadius="lg"
+                  alt="Status"
+                />
+              </Box>
+            </Box>
+
+            <Box>
+              <Box pt={1} pb={1}>
+                <Text color={"#F8784A"}>
+                  敵キャラクターに付与されているバフ
+                </Text>
+              </Box>
+              <Text>
+                与ダメージの計算に使用されるバフは以下です。適用されている各バフは、「味方キャラクターに付与されているバフ」と同じ画面で確認できます。
+              </Text>
+              <Box p={2}>
+                <UnorderedList fontSize="sm">
+                  <ListItem>デフェンスアップ</ListItem>
+                  <ListItem>ダメージカット</ListItem>
+                </UnorderedList>
+              </Box>
+            </Box>
+
+            <Box>
+              <Box pt={1} pb={1}>
+                <Text color={"#F8784A"}>
+                  敵キャラクターに付与されているデバフ
+                </Text>
+              </Box>
+              <Text>
+                与ダメージの計算に使用されるデバフは以下です。適用されている各バフは、「味方キャラクターに付与されているバフ」と同じ画面で確認できます。
+              </Text>
+              <Box p={2}>
+                <UnorderedList fontSize="sm">
+                  <ListItem>デフェンスダウン</ListItem>
+                </UnorderedList>
+              </Box>
+            </Box>
+
+            <Box>
+              <Box pt={2} pb={2}>
+                <Text color={"#F8784A"}>与ダメージの計算式</Text>
+              </Box>
+              <Box pb={2}>
+                <Text>
+                  はじめにパワー合計値を計算します。この値は、ダメージ計算の基礎となる値です。
+                </Text>
+                <Box pt={2} pb={2}>
+                  <Alert status="warning">
+                    <Text>
+                      パワー合計値 = キャラクターパワー +
+                      キャラクターアドバンスパワー + メモリーパワー
+                    </Text>
+                  </Alert>
+                </Box>
+              </Box>
+
+              <Text>
+                次に通常攻撃の与ダメージを計算します。謎の定数は気にしなくても大丈夫です。
+              </Text>
+              <Box pt={2} pb={2}>
+                <Alert status="warning">
+                  <Text>
+                    通常攻撃の与ダメージ = パワー合計値 * パワーアップ /
+                    (ディフェンスアップ + ディフェンスダウン) / ダメージカット /
+                    謎の定数
+                  </Text>
+                </Alert>
+              </Box>
+
+              <Text>最後にアクションスキル威力の与ダメージを計算します。</Text>
+              <Box pt={2} pb={2}>
+                <Alert status="warning">
+                  <Text>
+                    アクションスキル威力の与ダメージ = 通常攻撃の与ダメージ *
+                    アクションスキルダメージ率 * スキル威力 *
+                    アクションスキル威力
+                  </Text>
+                </Alert>
+              </Box>
+
+              <Text>
+                上記の与ダメージ計算の記載に関する注意事項です。一部、わかりやすさを優先して正確ではない記載をしています。需要はないと思いますが、正確な計算式を知りたい方は、お問合せください。
+              </Text>
+            </Box>
+
+            <Box pt={2} pb={2}>
+              <Text>
+                与ダメージの計算式がわかったので、実際に{" "}
+                <Link
+                  color="#FAC00F"
+                  href={"/calculation"}
+                  textDecoration={"underline"}
+                >
+                  与ダメージ計算
+                </Link>{" "}
+                で計算してみましょう。
+              </Text>
+            </Box>
           </Box>
         </Box>
 
         <Box p={2}>
           <Box>
             <Box p={2}>
-              <Heading color={"#F8784A"}>About Me</Heading>
+              <Heading color={"#F8784A"}>開発者について</Heading>
             </Box>
           </Box>
 
@@ -113,16 +293,18 @@ export default function Index() {
             p={4}
           >
             <Text>
-              If you have any questions, please contact me on{" "}
+              改善要望・ご質問・与ダメージ計算の結果に間違いがある場合は DM
+              等でご連絡頂けると嬉しいです{" "}
+            </Text>
+            <Text>
               <Link
                 color="#FAC00F"
-                href={"https://twitter.com/doz13189"}
+                href={"https://twitter.com/h_surround"}
                 textDecoration={"underline"}
                 isExternal
               >
-                my Twitter account
+                My X (Twitter) Account
               </Link>
-              .
             </Text>
 
             {/*  */}
---
+++ src/pages/question.tsx
@@ -0,0 +1,68 @@
+import AbilityDisplayBox from "@/components/AbilityDisplayBox";
+import DamageDisplayBox from "@/components/DamageDisplayBox";
+import { Heading } from "@/components/parts/Heading";
+import {
+  calculateNonCriticalActionSkillAttack,
+  calculateNonCriticalNormalAttack,
+} from "@/lib/calculatinLogic";
+import { getCharacter } from "@/lib/character";
+import { Box, LinkBox, LinkOverlay, Text } from "@chakra-ui/react";
+import { InferGetStaticPropsType } from "next";
+
+export async function getServerSideProps() {
+  const allCharactersData = getCharacter();
+  return {
+    props: {
+      allCharactersData,
+    },
+  };
+}
+
+export default function Character({
+  allCharactersData,
+}: InferGetStaticPropsType<typeof getServerSideProps>) {
+  const advancePower = 1800;
+  const characterKeys = Object.keys(allCharactersData[0]) as Array<
+    keyof (typeof allCharactersData)[0]
+  >;
+
+  return (
+    <main>
+      <Box p={2}>
+        <Box p={2}>
+          <Heading color={"#F8784A"}>よくある質問</Heading>
+        </Box>
+      </Box>
+
+      <Box p={2}>
+        <Box
+          backgroundColor={"blackAlpha.100"}
+          borderColor={"blackAlpha.600"}
+          rounded="lg"
+          p={4}
+        >
+          <Text>
+            疑問)プルスウルトラの与ダメージ計算をしたいのですが、どうすればいいですか？
+          </Text>
+          <Box pt={2} pb={2}>
+            <Text>回答) 現在、実装してません。</Text>
+          </Box>
+        </Box>
+      </Box>
+
+      <Box color={"64C8EF"} pt={4}>
+        <LinkBox
+          borderWidth="2px"
+          rounded="lg"
+          borderColor={"blackAlpha.600"}
+          backgroundColor={"#FAC00F"}
+          p={3}
+        >
+          <Heading pt={2} pb={2}>
+            <LinkOverlay href="/">ホームへ</LinkOverlay>
+          </Heading>
+        </LinkBox>
+      </Box>
+    </main>
+  );
+}
